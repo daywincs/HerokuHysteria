e5514dfeac0f3f1d3b1a0035737ea49ab8309373
daywincs <daywincs@gmail.com>
2022-05-08 23:08:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,19 +5,8 @@ mkdir -p /etc/hysteria
 wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/daywincs/HerokuHysteria/blob/main/hysteria-linux-amd64
 wget -q -O /etc/hysteria/ca.crt --no-check-certificate https://github.com/daywincs/HerokuHysteria/blob/main/ca.crt
 wget -q -O /etc/hysteria/ca.key --no-check-certificate https://github.com/daywincs/HerokuHysteria/blob/main/ca.key
+wget -q -O /etc/hysteria/config.json --no-check-certificate https://github.com/daywincs/HerokuHysteria/blob/main/config.json
 
 chmod 755 /etc/hysteria/hysteria
-
-
-cat <<EOF > /etc/hysteria/config.json
-{
-  "listen": ":9527",
-  "cert": "/etc/hysteria/ca.crt",
-  "key": "/etc/hysteria/ca.key",
-  "obfs": "password",
-  "up_mbps": 100,
-  "down_mbps": 100
-}
-EOF
 
 ./etc/hysteria/hysteria -c config.json server
